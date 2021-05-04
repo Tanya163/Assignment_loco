aec0c29814d05049bc07b58837ea721e77a66e3d
Apple <apple@Apples-MacBook-Air.local>
2021-05-04 19:17:27 +0530

    file changed

--- scripts/prog4.rb
+++ scripts/prog4.rb
@@ -1,6 +1,5 @@
 class String
     def palindrome?
-        puts self.reverse
         if self==self.reverse
             return true
         else
@@ -8,15 +7,15 @@ class String
         end
     end
 end
-new_str = gets
+new_str = gets.chomp
 p new_str.palindrome?
-# if new_str === ""
-#     puts "Please provide an input"
-# elsif new_str.palindrome?
-#     puts "Input string is a palindrome"
-# else
-#     puts "Input string is not a palindrome"
-# end
+if new_str === ""
+    puts "Please provide an input"
+elsif new_str.palindrome?
+    puts "Input string is a palindrome"
+else
+    puts "Input string is not a palindrome"
+end
     
 
 
